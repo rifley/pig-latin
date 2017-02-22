3e6025f9f5886d21cec802c81464ad2faab334eb
Chris Wainner and Riley Watts <chris@>
2017-02-22 15:38:33 -0800

    Update for regex, add y as vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,40 @@
 var pigLatinTranslator = function(input){
-  var temp;
-  var result;
+  var result, temp;
 
-  if(input.match(/^[aeiou]/gi)){
-    return input + "ay";
-  }
+  var tempArray = input.split(" ");
+  console.log(tempArray);
 
-  for(var index = 0; index < input.length; index++){
-    if(input.charAt(index).match(/[^aeiou]/gi)){
-      if((input.charAt(index+1) === "a")|| (input.charAt(index+1) === "e") || (input.charAt(index+1) === "i") ||(input.charAt(index+1) === "o")){
-        temp = input.slice(0, index+1);
-        result = input.slice(index+1) + temp;
-        console.log(result);
-      } else if(input.charAt(index).match(/[qu]/i)){
-        temp = input.slice(0, index + 2);
-        result = input.slice(index + 2) + temp;
-      } else if(input.charAt(index+1) === "u"){
-        temp = input.slice(0, index+1);
-        result = input.slice(index+1) + temp;
-        console.log(result);
+  for(var i = 0; i < tempArray.length; i++){
+    var word = tempArray[i];
+
+    if(word.match(/^[aeiou]/i)){
+      word += "ay";
+    } else{
+      for(var index = 0; index < word.length; index++){
+        console.log(word);
+          if(word.charAt(index+1).match(/[aeiouy]/i)){
+            temp = word.slice(0, index+1);
+            word = word.slice(index+1) + temp;
+            console.log(word);
+            index = word.length;
+          } else if(word.charAt(index).match(/[qu]/i)){
+            temp = word.slice(0, index + 2);
+            word = word.slice(index + 2) + temp;
+            index = word.length;
+          } else if(word.charAt(index+1) === "u"){
+            temp = word.slice(0, index+1);
+            word = word.slice(index+1) + temp;
+            console.log(word);
+            index = word.length;
+          }
+          console.log("found a consonant!" + word.charAt(index));
       }
-      console.log("found a consonant!" + input.charAt(index));
+      word += "ay";
     }
+    tempArray[i] = word;
   }
 
-  result = result + "ay";
+  result = tempArray.join(" ");
 
   return result;
 }
