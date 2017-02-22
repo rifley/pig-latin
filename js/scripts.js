c0ce233c44e4b741dfb079838c040049b54403ba
Chris Wainner and Riley Watts <chris@>
2017-02-22 14:45:19 -0800

    Update with new logic to include situations using qu

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,28 @@ var pigLatinTranslator = function(input){
   var result;
 
   if(input.match(/^[aeiou]/gi)){
-    alert("Starts with a vowel!");
-  } else{
-
+    return input + "ay";
   }
 
   for(var index = 0; index < input.length; index++){
     if(input.charAt(index).match(/[^aeiou]/gi)){
-      if((input.charAt(index+1) === "a")|| (input.charAt(index+1) === "e") || (input.charAt(index+1) === "i") ||(input.charAt(index+1) === "o") || (input.charAt(index+1) === "u")){
+      if((input.charAt(index+1) === "a")|| (input.charAt(index+1) === "e") || (input.charAt(index+1) === "i") ||(input.charAt(index+1) === "o")){
         temp = input.slice(0, index+1);
-        console.log(temp);
+        result = input.slice(index+1) + temp;
+        console.log(result);
+      } else if(input.charAt(index).match(/[qu]/i)){
+        temp = input.slice(0, index + 2);
+        result = input.slice(index + 2) + temp;
+      } else if(input.charAt(index+1) === "u"){
+        temp = input.slice(0, index+1);
+        result = input.slice(index+1) + temp;
+        console.log(result);
       }
       console.log("found a consonant!" + input.charAt(index));
     }
   }
 
-  result = input + "ay";
+  result = result + "ay";
 
   return result;
 }
